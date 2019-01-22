ded21054a660b4d0031c2e37747574d00dfcf866
sunnyghosh48 <kas120992@gmail.com>
2019-01-22 10:19:22 +0530

    committ

--- SpringBootHelloWorld_JUnit/src/main/java/com/javainuse/SpringBootHelloWorldApplication.java
+++ SpringBootHelloWorld_JUnit/src/main/java/com/javainuse/SpringBootHelloWorldApplication.java
@@ -2,11 +2,16 @@ package com.javainuse;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class SpringBootHelloWorldApplication extends SpringBootServletInitializer {
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringBootHelloWorldApplication.class);
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootHelloWorldApplication.class, args);
 	}
