dcd148b8cefc5349c824b745b6dce04a51d1a4c5
sunnyghosh48 <kas120992@gmail.com>
2019-01-25 02:52:51 +0530

    Committ

--- SpringBootHelloWorld_JUnit/src/test/java/com/javainuse/test/TestWebApp.java
+++ SpringBootHelloWorld_JUnit/src/test/java/com/javainuse/test/TestWebApp.java
@@ -28,7 +28,7 @@ public class TestWebApp extends SpringBootHelloWorldTests {
 	public void testEmployee() throws Exception {
 		mockMvc.perform(get("/employee")).andExpect(status().isOk())
 				.andExpect(content().contentType("application/json;charset=UTF-8"))
-				.andExpect(jsonPath("$.name").value("emp1")).andExpect(jsonPath("$.designation").value("manager"))
+				.andExpect(jsonPath("$.name").value("emp")).andExpect(jsonPath("$.designation").value("manager"))
 				.andExpect(jsonPath("$.empId").value("1")).andExpect(jsonPath("$.salary").value(3000));
 
 	}
