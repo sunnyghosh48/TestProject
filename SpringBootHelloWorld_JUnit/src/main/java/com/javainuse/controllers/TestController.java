d3eefafafc2ff1b435349afb1eddeb5c928fc920
sunnyghosh48 <kas120992@gmail.com>
2019-01-31 02:18:14 +0530

    committ

--- SpringBootHelloWorld_JUnit/src/main/java/com/javainuse/controllers/TestController.java
+++ SpringBootHelloWorld_JUnit/src/main/java/com/javainuse/controllers/TestController.java
@@ -13,7 +13,7 @@ public class TestController {
 	public Employee firstPage() {
 
 		Employee emp = new Employee();
-		emp.setName("emp1");
+		emp.setName("emp");
 		emp.setDesignation("manager");
 		emp.setEmpId("1");
 		emp.setSalary(3000);
